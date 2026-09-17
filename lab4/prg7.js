98687cf1425876609ba8131b6dee1fe99a63c1c5
abhayjeet chauhan <abhayjeetchauhan415@gmail.com>
2026-09-17 12:22:37 +0530

    add user and get user

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,20 +1,50 @@
 import http from "http";
+import { getUsers } from "./users.js";
 
 const server = http.createServer((req, res) => {
   if (req.url === "/api/users" && req.method === "GET") {
-    res.end(JSON.stringify({ msg: "all users" }));
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(getUsers()));
   } else if (req.url === "/api/users" && req.method === "POST") {
-    res.end(JSON.stringify({ msg: "add user" }));
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+      const user = JSON.parse(body);
+
+      console.log("POST REQUEST RECEIVED");
+      console.log(user);
+
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(user));
+    });
   } else if (req.url === "/api/users/1" && req.method === "GET") {
-    res.end(JSON.stringify({ msg: "single user with id 1" }));
+    res.end(
+      JSON.stringify({
+        msg: "single user with id 1",
+      }),
+    );
   } else if (req.url === "/api/users/1" && req.method === "PUT") {
-    res.end(JSON.stringify({ msg: "update user 1" }));
+    res.end(
+      JSON.stringify({
+        msg: "update user with id 1",
+      }),
+    );
   } else if (req.url === "/api/users/1" && req.method === "DELETE") {
-    res.end(JSON.stringify({ msg: "remove 1" }));
+    res.end(
+      JSON.stringify({
+        msg: "remove user with id 1",
+      }),
+    );
   } else {
     res.statusCode = 404;
     res.end();
   }
 });
 
-server.listen(3000, () => console.log("prg7 is running"));
+server.listen(3000, () => {
+  console.log("server is running at 3000...");
+});
--- lab4/users.js
+++ lab4/users.js
@@ -1,10 +1,9 @@
-// be use in memory data base
-// We use in memory data base 
-let users =[
-    {id:1,name:"Amit Sharma",mob:"98345xxxxx",email:"amit.example@exam.com"},
-    {id:2,name:"Monika Verma",mob:"92345xxxxx",email:"moni.example@exam.com"},
+// we use in memory database
+let users = [
+    {id:1,name:'Amit Sharma',mob:'98345xxxxx',email:'amit.example@exam.com'},
+    {id:2,name:'Monika Verma',mob:'92345xxxxx',email:'moni.example@exam.com'},
 ]
 
-let nextId=3;
+let nextId = 3;
 
 export const getUsers = () => users;

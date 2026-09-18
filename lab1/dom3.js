8ad06e8bc65bc385fe148528371a382c22ec7ff3
abhayjeet chauhan <abhayjeetchauhan415@gmail.com>
2026-09-18 14:33:46 +0530

    third file

--- lab1/dom3.js
+++ lab1/dom3.js
@@ -1,9 +1,23 @@
-import{EventEmitter} from "events";
-const button =new EventEmitter();
-button.on("click",(uname)=>{
-    console.log(`button clicked by ${uname}`);
+// import{EventEmitter} from "events";
+// const button =new EventEmitter();
+// button.on("click",(uname)=>{
+//     console.log(`button clicked by ${uname}`);
+// });
+// button.emit("clicked","Abhay jeet");
+// button.emit("clicked", "Abhay ");
+
+// button.emit("clicked", "Rajnesh");
+
+// button.emit("clicked", "Abhay jeet");
+
+// button.emit("clicked", "Abhay jeet");
+// button.emit("click");
+import { EventEmitter } from "events";
+const button = new EventEmitter();
+button.on("click", (uname) => {
+  console.log(`button clicked by ${uname}`);
 });
-button.emit("clicked","Abhay jeet");
+button.emit("clicked", "Abhay jeet");
 button.emit("clicked", "Abhay ");
 
 button.emit("clicked", "Rajnesh");

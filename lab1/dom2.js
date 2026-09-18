d0324fae3459e8152448869abb188bd9b63e44ba
abhayjeet chauhan <abhayjeetchauhan415@gmail.com>
2026-09-18 14:31:53 +0530

    second

--- lab1/dom2.js
+++ lab1/dom2.js
@@ -1,11 +1,22 @@
-import {EventEmitter}from "events";
-const button =new EventEmitter();
-button.on("click",()=>{
-    console.log("Task 1");
+// import {EventEmitter}from "events";
+// const button =new EventEmitter();
+// button.on("click",()=>{
+//     console.log("Task 1");
+// });
+// button.on("click", () => {
+//   console.log("Task 2");
+// });
+
+// button.emit("click");
+// jkfdsjovcoivckmc
+import { EventEmitter } from "events";
+const button = new EventEmitter();
+button.on("click", () => {
+  console.log("Task 1");
 });
 button.on("click", () => {
   console.log("Task 2");
 });
 
 button.emit("click");
-jkfdsjovcoivckmc
+jkfdsjovcoivckmc;

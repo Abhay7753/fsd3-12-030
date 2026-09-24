8de7ac17bd9b8d2b13d538199bccfc736b091d58
abhayjeet chauhan <abhayjeetchauhan415@gmail.com>
2026-09-24 12:18:38 +0530

    user add

--- lab4/users.js
+++ lab4/users.js
@@ -1,9 +1,43 @@
 // we use in memory database
+// We use in-memory database
 let users = [
-    {id:1,name:'Amit Sharma',mob:'98345xxxxx',email:'amit.example@exam.com'},
-    {id:2,name:'Monika Verma',mob:'92345xxxxx',email:'moni.example@exam.com'},
-]
+  {
+    id: 1,
+    name: "Amit Sharma",
+    mob: "98345xxxxx",
+    email: "amit.example@exam.com",
+  },
+  {
+    id: 2,
+    name: "Monika Verma",
+    mob: "92345xxxxx",
+    email: "moni.example@exam.com",
+  },
+];
 
 let nextId = 3;
 
-export const getUsers = () => users;
+const getAllUsers = () => {
+  return users;
+};
+
+const getUsersById = (pid) => {
+  const found = users.find((user) => user.id === pid);
+  return found;
+};
+
+export const addUser = (user) => {
+  user.id = nextId++;
+  users.push(user);
+
+  return user;
+};
+const updateUser=(pid,updateData)=>{
+  const index = users.findIndex((user)=> user.id === pid);
+  if (index ==-1){
+    return false;
+  }
+updateData.id =pid;
+user[index]=updateData;
+return updateData;
+}
